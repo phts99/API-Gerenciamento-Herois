1f96dac72375614fcb05202be5a29f53b0540e65
Pedro Trindade <phts99@gmail.com>
2021-05-06 20:22:14 -0300

    Init

---
+++ src/main/java/com/digitalinnovationone/heroesapi/config/HeroesData.java
@@ -0,0 +1,55 @@
+package com.digitalinnovationone.heroesapi.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import static com.digitalinnovationone.heroesapi.constants.HeroesConstant.REGION_DYNAMO;
+import static com.digitalinnovationone.heroesapi.constants.HeroesConstant.ENDPOINT_DYNAMO;
+
+public class HeroesData {
+    public static void main (String [] args) throws Exception{
+        AmazonDynamoDB client = AmazonDynamoDBAsyncClientBuilder.standard()
+        .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(REGION_DYNAMO,ENDPOINT_DYNAMO))
+        .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("Heroes_Table");
+        Item hero = new Item()
+            .withPrimaryKey("id", 1)
+            .withString("name", "Wonder Woman")
+            .withString("universe", "DC Comics")
+            .withNumber("films", 3);
+
+        Item hero2 = new Item()
+                .withPrimaryKey("id", "2")
+                .withString("name", "Spider Man")
+                .withString("universe", "Marvel")
+                .withNumber("films", 2);
+
+        Item hero3 = new Item()
+                .withPrimaryKey("id", "3")
+                .withString("name", "Hulk")
+                .withString("universe", "Marvel")
+                .withNumber("films", 2);
+
+        PutItemOutcome outcome = table.putItem(hero);
+    }
+}
---
+++ src/test/java/com/digitalinnovationone/heroesapi/HeroesapiApplicationTests.java
@@ -0,0 +1,56 @@
+package com.digitalinnovationone.heroesapi;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.digitalinnovationone.heroesapi.document.Heroes;
+import com.digitalinnovationone.heroesapi.repository.HeroesRepository;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+
+import static com.digitalinnovationone.heroesapi.constants.HeroesConstant.HEROES_ENDPOINT_LOCAL;
+
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
+@SpringBootTest
+class HeroesapiApplicationTests {
+
+	@Autowired
+	WebTestClient webTestClient;
+
+	@Autowired
+	HeroesRepository heroesRepository;
+
+	@Test
+	public void getOneHeroById(){
+		webTestClient.get().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"),"1")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody();
+	}
+
+	@Test
+	public void getOneHeroNotFound(){
+		webTestClient.get().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"),"10")
+				.exchange()
+				.expectStatus().isNotFound();
+	}
+
+	@Test
+	public void deleteHero(){
+		webTestClient.delete().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"),"3")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isNotFound()
+				.expectBody(Void.class);
+	}
+}
